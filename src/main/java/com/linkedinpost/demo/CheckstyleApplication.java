19a2a44688b079d14ffd92987919c88d4705580b
joshisidharth12 <joshisidharth12@gmail.com>
2024-10-13 13:35:47 +0530

    Added basic files

---
+++ src/main/java/com/linkedinpost/demo/CheckstyleApplication.java
@@ -0,0 +1,13 @@
+package com.linkedinpost.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckstyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckstyleApplication.class, args);
+	}
+
+}
